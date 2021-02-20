fe95b422b1f9b49c9618551c344789cc6a555989
scrowley016 <33128271+scrowley016@users.noreply.github.com>
2021-02-20 11:42:06 -0500

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Switch
+  } from 'react-router-dom'
+
+
+  const App =()=>{
+      return(
+          <Router>
+              <Switch>
+
+              </Switch>
+          </Router>
+      )
+  }
